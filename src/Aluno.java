239a2d21c30b5c3115f77075696604945fce2c6a
Wagner Marques <marquesmwagner@gmail.com>
2022-04-10 15:28:05 -0300

    Fix toString method

--- src/Aluno.java
+++ src/Aluno.java
@@ -4,7 +4,8 @@ public class Aluno extends Pessoa {
 
     private double notaFinal;
 
-    public Aluno(String nome, String telefone, String dataNascimento, LocalDate dataCadastro, LocalDate dataAlteracao,
+    public Aluno(String nome, String telefone, String dataNascimento, LocalDate dataCadastro,
+            LocalDate dataAlteracao,
             double notaFinal) {
         super(nome, telefone, dataNascimento, dataCadastro, dataAlteracao);
         this.notaFinal = notaFinal;
@@ -20,13 +21,12 @@ public class Aluno extends Pessoa {
 
     @Override
     public String toString() {
-        return
-                "Nome: " + getNome() +
-                ", Telefone: " + getTelefone() +
-                ", Data de Nascimento: " + getDataNascimento() +
-                ", Data de Cadastro: " + getDataCadastro() +
-                ", Data de Alteração: " + getDataAlteracao() +
-                ", Notal Final: " + getNotaFinal();
+        return "Nome: " + getNome() +
+                " | Telefone: " + getTelefone() +
+                " | Data de Nascimento: " + getDataNascimento() +
+                " | Data de Cadastro: " + getDataCadastro() +
+                " | Data de Alteração: " + getDataAlteracao() +
+                " | Notal Final: " + getNotaFinal() + "\n";
     }
 
 }
